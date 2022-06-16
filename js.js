45a08792cd4f6a2f3642ef64788a3169a92e8f2b
Hendrick201 <81498960+Hendrick201@users.noreply.github.com>
2022-06-16 18:02:21 -0300

    Add files via upload

--- js.js
+++ js.js
@@ -7,5 +7,5 @@ function catchData()
     addres = document.getElementById("addres").value +"";
     addresNumber = document.getElementById("addresNumber").value +"";
     district = document.getElementById("district").value +"";
-    document.getElementById("finalLink").href="https://api.whatsapp.com/send?phone=553498824016&text=Olá,%20tudo%20bem?%0asegue%20abaixo%20os%20meus%20dados:" + "%0a" + name + telNumber + "%0a" + addres + "," + "%20" + addresNumber +"," + "%20" + district +"."; 
+    document.getElementById("finalLink").href="https://api.whatsapp.com/send?phone=553498824016&text=Olá,%20tudo%20bem?%0asegue%20abaixo%20os%20meus%20dados:" + "%0a" + name + "%0a" + telNumber + "%0a" + addres + "," + "%20" + addresNumber +"," + "%20" + district +"."; 
 }
