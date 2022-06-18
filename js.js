0fa4ac439d919dd54cc9b926923f220a37662e97
Hendrick201 <81498960+Hendrick201@users.noreply.github.com>
2022-06-17 22:00:14 -0300

    Add files via upload

--- js.js
+++ js.js
@@ -1,11 +1,19 @@
 
 function catchData()
 {
-    var name, telNumber, addres, addresNumber, district;
+    
+    var name, telNumber, city, addres, addresNumber, district;
     name = document.getElementById("name").value +"";
+    city = document.getElementById("city").value + "";
     telNumber = document.getElementById("telNumber").value +"";
     addres = document.getElementById("addres").value +"";
     addresNumber = document.getElementById("addresNumber").value +"";
     district = document.getElementById("district").value +"";
-    document.getElementById("finalLink").href="https://api.whatsapp.com/send?phone=553498824016&text=Olá,%20tudo%20bem?%0asegue%20abaixo%20os%20meus%20dados:" + "%0a" + name + "%0a" + telNumber + "%0a" + addres + "," + "%20" + addresNumber +"," + "%20" + district +"."; 
+    document.getElementById("finalLink").href="https://api.whatsapp.com/send?phone=553498824016&text=Olá,%20tudo%20bem?%0asegue%20abaixo%20os%20meus%20dados:" + "%0a" + name + "%0a" + telNumber + "%0a" + city + "%0a" + addres + "," + "%20" + addresNumber +"," + "%20" + district +"."; 
+}
+function enableButton()
+{
+    var button = document.getElementById("finalLink");
+    var buttonBlinder = document.getElementById("finalLinkBlinder");
+
 }
